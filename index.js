a2236a08e0290199d9e0fd167a3acd8b87e9572e
Juga Paazmaya <paazmaya@yahoo.com>
2014-01-22 14:53:31 +0200

    Writing out of head...

--- index.js
+++ index.js
@@ -5,13 +5,17 @@
 
 
 var express = require('express');
-var app = express();
+var
 
+var app = express();
 app.use(express.logger());
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
 app.post('/translate', function(req, res){
+  res.send('{"hello": "world"}');
+});
+app.post('/save-translation', function(req, res) {
   res.send('{"hello": "world"}');
 });
 
--- static/js/bunkai-kumite.js
+++ static/js/bunkai-kumite.js
@@ -4,7 +4,65 @@
  */
 
 (function () {
-  var Matsumura = {};
+  var Matsumura = {
+
+    /**
+     * Create an input to a table cell if it does not already have it,
+     * and set one time event handlers (enter/esc)
+     * @param {Zepto} $td A td element wrapped as a Zepto object
+     */
+    createInlineInput: function ($td) {
+      var text = $.trim($td.text() || '');
+      var lang = $td.attr('data-lang');
+      var id = $td.parents('tr').attr('data-id');
+
+      // insert input which is removed once its data has been sent via enter key
+      var tmpl = $('#text_input');
+      var html = Hogan.compile(tmpl.html(), { id: id, lang: lang, text: text });
+
+      // Send if enter key comes up later, remove if esc.
+      $td.html(html).find('input').focus().on('keyup', this.onInputKeyup);
+      $td.attr('data-original', text);
+    },
+
+    onInputKeyup: function (event) {
+      console.dir(event);
+      var $input = $(event.currentTarget);
+      var $td = $input.parents('td');
+
+      if (event.which === 13) { // enter
+        event.preventDefault();
+
+        var content = $.trim($input.val());
+        this.saveTranslation($input.attr('data-id'), $input.attr('lang'), content, $td);
+      }
+      else if (event.which === 27) { // esc
+        // TODO: remove keyup listener if seems hanging...
+        $td.remove('.form');
+        $td.text($td.attr('data-original'));
+        $td.attr('data-original', null);
+      }
+    },
+
+    /**
+     * Save the given translation to the PO file
+     */
+    saveTranslation: function (id, lang, content, $td) {
+
+      var data = {
+        id: id,
+        lang: lang,
+        content: content
+      };
+
+      $.post('/save-translation', data, function (incoming, textStatus) {
+        // TODO: Perhaps should check what is the level of success?
+        $td.remove('.form');
+        $td.text(content);
+      }, 'json');
+    }
+
+  };
 
   $('button').on('click', function (event) {
     event.preventDefault();
