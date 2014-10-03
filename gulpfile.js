8cc754ea3eb393d128ae1604f66985e3b2bec849
Juga Paazmaya <paazmaya@yahoo.com>
2014-10-03 12:33:35 +0300

    Just would need to write first tests and try them on testling... #4

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,9 @@ var nodeunitRunner = require("gulp-nodeunit-runner");
 // https://github.com/jonkemp/gulp-qunit
 var qunit = require('gulp-qunit');
 
+// https://github.com/sindresorhus/gulp-mocha
+var mocha = require('gulp-mocha');
+
 gulp.task('lint', function () {
   gulp.src(['*.js', 'lib/*.js', 'static/js/bunkai-kumite.js', '!node_modules/**'])
     .pipe(eslint({config: 'eslint.json'}))
@@ -27,4 +30,7 @@ gulp.task('test', function () {
 
   gulp.src('tests/qunit/test-runner.html')
     .pipe(qunit());
+
+  gulp.src(['tests/mocha/*_spec.js'], {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
 });
